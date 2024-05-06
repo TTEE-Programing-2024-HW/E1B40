609fa880de0350a0746cb9e5542fb68878930c55
ChenBrenden <167835925+ChenBrenden@users.noreply.github.com>
2024-05-06 22:53:38 +0800

    新增個人頁面

---
+++ HW2.1.c
@@ -0,0 +1,68 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main(void) {
+    int a = 0, b = 0, pass, i, j, f, g;
+    char ch, h;
+    
+    printf("設計自己的封面\n");
+    puts("EEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEE");
+puts("EEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEE");
+puts("EE                                                        EE");
+puts("EE               eeeeeeeeeeeeeeeee                        EE");
+puts("EE               e                                        EE");
+puts("EE               e                                        EE");
+puts("E                e                                        EE");
+puts("EE               eeeeeeeeeeeeeeeee                        EE");
+puts("EE               e                                        EE");
+puts("EE               e                                        EE");
+puts("EE               e                                        EE");
+puts("EE               eeeeeeeeeeeeeeeee                        EE");
+puts("EE                                                        EE");
+puts("EE                        11                              EE");
+puts("EE                       111                              EE");
+puts("EE                     11111                              EE");
+puts("EE                        11                              EE");
+puts("EE                        11                              EE");
+puts("EE                        11                              EE");
+puts("EE                        11                              EE");
+puts("EE                 1111111111111111                       EE");
+puts("EE                                                        EE");
+puts("EE              bbbbbbbbbbbbbbbbbb                        EE");
+puts("EE              b                 b                       EE");
+puts("EE              b                  b                      EE");
+puts("EE              b                 b                       EE");
+puts("EE              bbbbbbbbbbbbbbbbbb                        EE");
+puts("EE              b                 b                       EE");
+puts("EE              b                  b                      EE");
+puts("EE              b                 b                       EE");
+puts("EE              bbbbbbbbbbbbbbbbbb                        EE");
+puts("EE                                                        EE");
+puts("EE                                                        EE");
+puts("EE                       444                              EE");
+puts("EE                     44444                              EE");
+puts("EE                  44  4444                              EE");
+puts("EE                4444  4444                              EE");
+puts("EE              4444444444444444                          EE");
+puts("EE                       444                              EE");
+puts("EE                       444                              EE");
+puts("EE                       444                              EE");
+puts("EE                       444                              EE");
+puts("EE                                                        EE");
+puts("EE                                                        EE");
+puts("EE                0000000000000000                        EE");
+puts("EE                00            00                        EE");
+puts("EE                00            00                        EE");
+puts("EE                00            00                        EE");
+puts("EE                00            00                        EE");
+puts("EE                00            00                        EE");
+puts("EE                00            00                        EE");
+puts("EE                00            00                        EE");
+puts("EE                00            00                        EE");
+puts("EE                00            00                        EE");
+puts("EE                0000000000000000                        EE");
+puts("EE                                                        EE");
+puts("EEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEE");
+puts("EEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEE");
+return 0;
+}
