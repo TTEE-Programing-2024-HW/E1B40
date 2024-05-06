132ea68fb81145dd07706bfe37ba7ad38d956d11
ChenBrenden <167835925+ChenBrenden@users.noreply.github.com>
2024-05-06 17:26:49 +0800

    第一次上傳試試看

---
+++ hw2.c
@@ -0,0 +1,109 @@
+#include<stio.h>
+#include<stdlib.h>
+
+int main(void){
+	
+int a,b=0,c,d,f,i,j,k;
+char g,e,h;
+	
+puts("EEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEE");
+puts("EEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEE");
+puts("EE                                                        EE");
+puts("EE               eeeeeeeeeeeeeeeee                        EE");
+puts("EE               e                                        EE");
+puts("EE               e                                        EE");
+puts("E                e                                        EE");
+puts("EE               eeeeeeeeeeeeeeeee                        EE");
+puts("EE               e                                        EE");
+puts("EE               e                                        EE");
+puts("EE               e                                        EE");
+puts("EE               eeeeeeeeeeeeeeeee                        EE");
+puts("EE                                                        EE");
+puts("EE                        11                              EE");
+puts("EE                       111                              EE");
+puts("EE                     11111                              EE");
+puts("EE                        11                              EE");
+puts("EE                        11                              EE");
+puts("EE                        11                              EE");
+puts("EE                        11                              EE");
+puts("EE                 1111111111111111                       EE");
+puts("EE                                                        EE");
+puts("EE              bbbbbbbbbbbbbbbbbb                        EE"); 
+puts("EE              b                 b                       EE");
+puts("EE              b                  b                      EE");
+puts("EE              b                 b                       EE");
+puts("EE              bbbbbbbbbbbbbbbbbb                        EE");
+puts("EE              b                 b                       EE");
+puts("EE              b                  b                      EE");
+puts("EE              b                 b                       EE");
+puts("EE              bbbbbbbbbbbbbbbbbb                        EE");
+puts("EE                                                        EE");
+puts("EE                                                        EE");
+puts("EE                       444                              EE");
+puts("EE                     44444                              EE");
+puts("EE                  44  4444                              EE");
+puts("EE                4444  4444                              EE");
+puts("EE              4444444444444444                          EE");
+puts("EE                       444                              EE");
+puts("EE                       444                              EE");
+puts("EE                       444                              EE");
+puts("EE                       444                              EE");
+puts("EE                                                        EE");
+puts("EE                                                        EE");
+puts("EE                0000000000000000                        EE");
+puts("EE                00            00                        EE");
+puts("EE                00            00                        EE");
+puts("EE                00            00                        EE");
+puts("EE                00            00                        EE");
+puts("EE                00            00                        EE");
+puts("EE                00            00                        EE");
+puts("EE                00            00                        EE");
+puts("EE                00            00                        EE");
+puts("EE                00            00                        EE");
+puts("EE                0000000000000000                        EE");
+puts("EE                                                        EE");
+puts("EEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEE");
+puts("EEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEE"); 
+//顯示個人化風格 
+
+while(1)//迴圈 (重複)1=true
+{
+	printf("請輸入四個數字:");
+	scanf("%d",&a);
+	if("a==2024")
+	{
+		break;
+	}
+	else
+	{
+		printf("錯誤\n");
+		b++;
+	}
+	if(b==3)
+	return 0;
+ } 
+    system("pause");//暫停程式 
+    while(1)
+	{
+		system("CLS"); //清除畫面
+		pruntf("| a.劃出n階直角三角形 |\n");
+		printf("| b.顯示九九乘法表    |\n");
+		printf("| c.結束              |\n");
+		printf("請輸入一個字元\n");
+		fflush(stdin);
+		   scanf("%s",&c);
+		   switch(c)
+		   {
+		     case 'a'://n階直角三角形
+			 case 'A':
+			   while(1)
+			   {
+			   	system("CLS");
+			   	printf("")
+				} 
+	 } 
+
+
+}
+
+
