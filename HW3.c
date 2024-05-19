09502088c367013bc5344526ab47e6231206a42b
ChenBrenden <167835925+ChenBrenden@users.noreply.github.com>
2024-05-20 00:30:43 +0800

    第一次上傳 印出個人化風格

---
+++ HW3.c
@@ -0,0 +1,77 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <conio.h>
+#define MAX 20
+
+void menu(); // 宣告副程式
+void afunction(char main[10][10]);
+char bfunction(char main[10][10]);
+char cfunction(char main[10][10]);
+int main()
+{
+
+    int password, count = 0, i, j, x, y;
+    char ch, ch2;
+
+    printf("設計自己的封面\n");
+    // 顯示封面，這裡省略重複部分
+    // ... (省略長的封面代碼)
+    puts("EEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEE");
+    puts("EEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEE");
+    puts("EE                                                        EE");
+    puts("EE               eeeeeeeeeeeeeeeee                        EE");
+    puts("EE               e                                        EE");
+    puts("EE               e                                        EE");
+    puts("EE               e                                        EE");
+    puts("EE               eeeeeeeeeeeeeeeee                        EE");
+    puts("EE               e                                        EE");
+    puts("EE               e                                        EE");
+    puts("EE               e                                        EE");
+    puts("EE               eeeeeeeeeeeeeeeee                        EE");
+    puts("EE                                                        EE");
+    puts("EE                        11                              EE");
+    puts("EE                       111                              EE");
+    puts("EE                     11111                              EE");
+    puts("EE                        11                              EE");
+    puts("EE                        11                              EE");
+    puts("EE                        11                              EE");
+    puts("EE                        11                              EE");
+    puts("EE                 1111111111111111                       EE");
+    puts("EE                                                        EE");
+    puts("EE              bbbbbbbbbbbbbbbbbb                        EE");
+    puts("EE              b                 b                       EE");
+    puts("EE              b                  b                      EE");
+    puts("EE              b                 b                       EE");
+    puts("EE              bbbbbbbbbbbbbbbbbb                        EE");
+    puts("EE              b                 b                       EE");
+    puts("EE              b                  b                      EE");
+    puts("EE              b                 b                       EE");
+    puts("EE              bbbbbbbbbbbbbbbbbb                        EE");
+    puts("EE                                                        EE");
+    puts("EE                                                        EE");
+    puts("EE                       444                              EE");
+    puts("EE                     44444                              EE");
+    puts("EE                  44  4444                              EE");
+    puts("EE                4444  4444                              EE");
+    puts("EE              4444444444444444                          EE");
+    puts("EE                       444                              EE");
+    puts("EE                       444                              EE");
+    puts("EE                       444                              EE");
+    puts("EE                       444                              EE");
+    puts("EE                                                        EE");
+    puts("EE                                                        EE");
+    puts("EE                0000000000000000                        EE");
+    puts("EE                00            00                        EE");
+    puts("EE                00            00                        EE");
+    puts("EE                00            00                        EE");
+    puts("EE                00            00                        EE");
+    puts("EE                00            00                        EE");
+    puts("EE                00            00                        EE");
+    puts("EE                00            00                        EE");
+    puts("EE                00            00                        EE");
+    puts("EE                00            00                        EE");
+    puts("EE                0000000000000000                        EE");
+    puts("EE                                                        EE");
+    puts("EEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEE");
+    puts("EEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEE");
